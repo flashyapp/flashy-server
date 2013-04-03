95ca8c1ff4f854572ba476a50450b1ecafeb3d85
Nick Beaulieu <nsbeauli@princeton.edu>
2013-04-03 08:09:22 -0400

    changed root to something stupid to check apache environment variable settings

--- flashyserver.py
+++ flashyserver.py
@@ -6,49 +6,66 @@ import MySQLdb
 import os
 from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash, jsonify
 
+
+import os
+# configuration
+DATABASE = 'Default'
+DEBUG = True
+SECRET_KEY = 'developer key'
+USERNAME = 'dbUserName'
+PASSWORD = 'dbPassword'
+ALLOWED_IMAGE_EXTENSIONS = set(['jpeg', 'jpg', 'gif', 'png', 'bmp'])
+APP_ROOT = os.path.dirname(os.path.abspath(__file__))
+IMAGE_DIRECTORY = "user_images/"
+
+
+
+
 # create the application
 app = Flask(__name__)
-# app.config.from_object(__name__)
+app.config.from_object(__name__)
 
 app.config.from_envvar('FLASHY_SETTINGS', silent=True)
 
 def connect_db():
     return MySQLdb.connect(
-        user=app.config['USERNAME'],
-        passwd=app.config['PASSWORD'],
-        db=app.config['DATABASE'])
+	user=app.config['USERNAME'],
+	passwd=app.config['PASSWORD'],
+	db=app.config['DATABASE'])
 
 def init_db():
     from pprint import pprint
     with closing(connect_db()) as db:
-        with app.open_resource('schema.sql') as f:
-            cur = db.cursor()
-            str = f.read()
-            print str
-            cur.execute(str)
-            cur.close()
-        db.commit()
+	with app.open_resource('schema.sql') as f:
+	    cur = db.cursor()
+	    str = f.read()
+	    print str
+	    cur.execute(str)
+	    cur.close()
+	db.commit()
 
 @app.before_request
 def before_request():
+    print "before_request"
     g.db = connect_db()
 
 @app.teardown_request
 def teardown_request(exception):
+    print "teardown_request"
+    g.db.commit()
     g.db.close()
 
 @app.route('/')
-def show_entries():
-    cur = g.db.cursor()
-    cur.execute('SELECT title, filename FROM photos ORDER BY id DESC')
-    cur.close()
-    photos = cur.fetchall()
-    pprint(photos)
-    return jsonify(photos)
-
+def show_info():
+    return '''
+    <!doctype html>
+    <title> Dev Info </title>
+    SETTINGS Path: {0}'''.format(os.environ['FLASHY_SETTINGS'])
+  
 def allowed_file(filename):
     return '.' in filename and \
-           filename.rsplit('.', 1)[1] in app.config['ALLOWED_IMAGE_EXTENSIONS']
+	  filename.rsplit('.', 1)[1] in app.config['ALLOWED_IMAGE_EXTENSIONS']
+
 def create_filename(fid, filename):
     ext = filename.rsplit('.', 1)[1]
     fn = str(fid) + "." + ext
@@ -58,22 +75,21 @@ def create_filename(fid, filename):
 def add_image():
     file = request.files['file']
     if file and allowed_file(file.filename):
-        # get the maximum id number for the table
-        cur = g.db.cursor()
-        cur.execute('SELECT MAX(id) AS max_id FROM photos')
-        fid = cur.fetchall()[0][0]
-        if fid == None:
-            fid = 1
-        else:
-            fid += 1                # increment the max id number
-        filename = create_filename(fid, file.filename)
-        file.save(app.config['APP_ROOT'] +
-                  app.config['IMAGE_DIRECTORY']
-                  + filename)
-        cur.execute("INSERT INTO photos VALUES(NULL, '{0}')".format(filename))
-        cur.close()
-        g.db.commit()
-        return jsonify(filename = filename)
+	# get the maximum id number for the table
+	cur = g.db.cursor()
+	cur.execute('SELECT MAX(id) AS max_id FROM photos')
+	fid = cur.fetchall()[0][0]
+	if fid == None:
+	    fid = 1
+	else:
+	    fid += 1                # increment the max id number
+	filename = create_filename(fid, file.filename)
+	file.save(app.config['APP_ROOT'] +
+		  app.config['IMAGE_DIRECTORY']
+		  + filename)
+	cur.execute("INSERT INTO photos VALUES(NULL, '{0}')".format(filename))
+	cur.close()
+	return jsonify(filename = filename)
 
 @app.route('/get_images', methods=['GET'])
 def get_images():
@@ -81,7 +97,6 @@ def get_images():
     cur.execute('SELECT * FROM photos')
     result = cur.fetchall()
     cur.close()
-    g.db.commit()
     return jsonify(result)
 
 if __name__ == "__main__":
